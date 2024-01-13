3c425948a5ec2b3f864d7c14c06d6c1461d24791
Michael Aroworade <Ayoaro85@gmail.com>
2024-01-13 03:45:59 -0800

    Square class updated, updated function created.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,8 +26,44 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
+        """Set the size of the square."""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Update the square.
+
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents size attribute
+                - 3rd argument represents x attribute
+                - 4th argument represents y attribute
+            **kwargs (dict): New key/value pairs of atributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
 
     def __str__(self):
         """Return the print() and str() representation of a Square."""
