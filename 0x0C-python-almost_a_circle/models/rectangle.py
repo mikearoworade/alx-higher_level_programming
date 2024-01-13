e1c3ac946c26bc6f97625dfab709092886d1707e
Michael Aroworade <Ayoaro85@gmail.com>
2024-01-13 04:39:32 -0800

    Rectangle class updated, to_dictionary function included.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -144,3 +144,13 @@ class Rectangle(Base):
                     self.x = v
                 elif k == "y":
                     self.y = v
+
+    def to_dictionary(self):
+        """Return the dictionary representation of the Square."""
+        return {
+                "id": self.id,
+                "width": self.width,
+                "height": self.height,
+                "x": self.x,
+                "y": self.y
+            }
