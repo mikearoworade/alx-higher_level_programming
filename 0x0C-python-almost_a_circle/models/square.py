797ddd2c49a3c2b7c1e575aa790a48543f00f66c
Michael Aroworade <Ayoaro85@gmail.com>
2024-01-13 02:27:14 -0800

    New class Square created, inherits Rectangle class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+# square.py
+# Michael Aroworade
+"""Define a square class."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new square
+
+        Ars:
+            size (int): The size of the new square.
+            x (int): The x coordinate of the new square.
+            y (int): The y coordinate of the new square.
+            id (int): The identity of the new square.
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        self.__width = value
+        self.__height = value
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
