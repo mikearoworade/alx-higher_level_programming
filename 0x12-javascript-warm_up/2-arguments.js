5a57690d011bd2fbe7a30949d7f114149c1a5970
Michael Aroworade <mike@mike.mike>
2024-03-21 18:09:07 +0100

    javascript warmup updated.

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,19 @@
 #!/usr/bin/node
 const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+console.log(
+  count === 2
+    ? "No argument"
+    : count === 3
+    ? "Argument found"
+    : "Arguments found"
+);
+
+// OR
+
+if (count === 2) {
+  console.log("No Argument.");
+} else if (count === 3) {
+  console.log("Argument Found.");
+} else {
+  console.log("Arguments Found.");
+}
--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,11 @@
 #!/usr/bin/node
 const num = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
+console.log(isNaN(num) ? "Not a number" : `My number: ${num}`);
+
+// OR
+
+if (isNaN(num)) {
+  console.log("Not a number");
+} else {
+  console.log(`My number: ${num}`);
+}
